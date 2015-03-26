8c95e9643465c21fee3faa1eec9d3578ad7a42bd
Evan O'Connell <oconn.e@gmail.com>
2015-03-26 18:39:46 -0400

    add ranks

--- examples/config/init.php
+++ examples/config/init.php
@@ -4,6 +4,7 @@ namespace RudiExample\Config;
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Rudi\Model\Soldier as Soldier;
+use Rudi\Model\Rank as Rank;
 
 include __DIR__.'/../vendor/autoload.php';
 
@@ -20,4 +21,31 @@ Capsule::schema()->create('soldiers', function($table)
 });
 
 Soldier::create(['name' => 'Rocky Marne']);
+
+print 'Creating ranks table'.PHP_EOL;
+
+Capsule::schema()->create('ranks', function($table)
+{
+    $table->increments('id');
+    $table->string('shortname');
+    $table->string('longname');
+    $table->string('img_filename');
+    $table->integer('ordinal');
+    $table->timestamps();
+});
+
+Rank::create(['shortname' => 'RCT', 'longname' => 'Recruit', 'img_filename' => 'RCT.png', 'ordinal' => 0]);
+Rank::create(['shortname' => 'PVT', 'longname' => 'Private', 'img_filename' => 'PVT.png', 'ordinal' => 1]);
+Rank::create(['shortname' => 'PV2', 'longname' => 'Private Second Class', 'img_filename' => 'PV2.png', 'ordinal' => 2]);
+Rank::create(['shortname' => 'PFC', 'longname' => 'Private First Class', 'img_filename' => 'PFC.png', 'ordinal' => 3]);
+Rank::create(['shortname' => 'SPC', 'longname' => 'Specialist', 'img_filename' => 'SPC.png', 'ordinal' => 4]);
+Rank::create(['shortname' => 'CPL', 'longname' => 'Corporal', 'img_filename' => 'CPL.png', 'ordinal' => 5]);
+Rank::create(['shortname' => 'SGT', 'longname' => 'Sergeant', 'img_filename' => 'SGT.png', 'ordinal' => 6]);
+Rank::create(['shortname' => 'SSG', 'longname' => 'Staff Sergeant', 'img_filename' => 'SSG.png', 'ordinal' => 7]);
+Rank::create(['shortname' => 'SFC', 'longname' => 'Sergeant First Class', 'img_filename' => 'SFC.png', 'ordinal' => 8]);
+Rank::create(['shortname' => 'MSG', 'longname' => 'Master Sergeant', 'img_filename' => 'MSG.png', 'ordinal' => 9]);
+Rank::create(['shortname' => '1SG', 'longname' => '1st Sergeant', 'img_filename' => '1SG.png', 'ordinal' => 10]);
+Rank::create(['shortname' => '2LT', 'longname' => '2nd Lieutenant', 'img_filename' => '2LT.png', 'ordinal' => 11]);
+Rank::create(['shortname' => '1LT', 'longname' => '1st Lieutenant', 'img_filename' => '1LT.png', 'ordinal' => 12]);
+Rank::create(['shortname' => 'CPT', 'longname' => 'Captain', 'img_filename' => 'CPT.png', 'ordinal' => 13]);
 
--- src/model/rank.php
+++ src/model/rank.php
@@ -6,4 +6,5 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Rank extends Eloquent {
 
+    protected $fillable = ['shortname', 'longname', 'img_filename', 'ordinal'];
 }
